3febeaa15612e094d31f29bb650ffc1d71451428
Leia Renée <leiarenee20@gmail.com>
2022-08-17 11:06:53 +0300

    plan to file

--- scripts/job_complete.sh
+++ scripts/job_complete.sh
@@ -3,8 +3,10 @@
 
 completed_module=$(echo $1 | sed s/$STACK_FOLDER\\\///g)
 echo
-echo TERRAGRUNT-PROCESS-COMPLETE $completed_module
-#pwd
+echo TERRAGRUNT COMMAND \"$TG_COMMAND\"
+echo SUB PROCESS $$ 
+echo MODULE COMPLETE \"$completed_module\"
+
 echo $completed_module >> $TG_PARRENT_DIR/completed_tasks.log
 completed_tasks=($(cat $TG_PARRENT_DIR/completed_tasks.log))
 module_count=${#completed_tasks[@]}
@@ -12,23 +14,36 @@ echo
 echo "Completed modules $module_count / $TG_MODULES_COUNT"
 echo ------------------------------------------------------
 echo 
-[ -z $SQS_QUEUE_URL ] && exit 0
+
+# Calculate percentage
 progress=$(($INITIAL_PROGRESS + ($module_count * ($MODULES_FINAL_PROGRESS - $INITIAL_PROGRESS) / $TG_MODULES_COUNT)))
+
 if [ $progress -gt $MODULES_FINAL_PROGRESS ]
 then
  progress=$MODULES_FINAL_PROGRESS
+ echo "Progress: $progress %"
 fi
 
-echo "Progress: $progress %"
-echo
-message_body="{\"message\":{\"status\":\"Module completed '$completed_module'\",\"progress\":$progress,\"module\":\"$completed_module\"}}"
-echo "Sending SQS Message $message_body" 
-echo SQS Queue URL $SQS_QUEUE_URL
-echo SQS Message Group ID $SQS_MESSAGE_GROUP_ID
-echo SQS_AWS_PROFILE $SQS_AWS_PROFILE
-echo completed_module : $completed_module
-echo
+# Prepare PLAN Files
+if [[ $TG_COMMAND == plan ]] && [[ -f plan-state-file ]]
+then
+  terraform show plan-state-file > plan-file.txt
+  terraform show -json plan-state-file > plan-file.json
+fi
 
-# send sqs message
-aws --profile $SQS_AWS_PROFILE sqs send-message --queue-url "$SQS_QUEUE_URL" --message-group-id "$SQS_MESSAGE_GROUP_ID" --message-body "$message_body"
+# SQS Message Handling
+if [[ ! -z $SQS_QUEUE_URL ]]
+then
+  # Print SQS variables
+  echo
+  message_body="{\"message\":{\"status\":\"Module completed '$completed_module'\",\"progress\":$progress,\"module\":\"$completed_module\"}}"
+  echo SQS Message Body $(echo "$message_body" | jq .)
+  echo SQS Queue URL $SQS_QUEUE_URL
+  echo SQS Message Group ID $SQS_MESSAGE_GROUP_ID
+  echo SQS_AWS_PROFILE $SQS_AWS_PROFILE
+  echo completed_module : $completed_module
+  echo
 
+  # send sqs message
+  aws --profile $SQS_AWS_PROFILE sqs send-message --queue-url "$SQS_QUEUE_URL" --message-group-id "$SQS_MESSAGE_GROUP_ID" --message-body "$message_body"
+fi
